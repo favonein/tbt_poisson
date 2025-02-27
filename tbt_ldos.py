659e66ee71d1cc54eb8ccc9855f14eac37478627
Favian Sun <sun.skybird@gmail.com>
2025-02-27 14:38:35 -0600

    Update tbt_ldos to include inputs

--- tbt_ldos.py
+++ tbt_ldos.py
@@ -1,18 +1,34 @@
 import os
-import sys
+import argparse
 import sisl
 import numpy as np
 import multiprocessing as mp
 
+dir_map = {'x': 0, 'a': 0, 'y': 0, 'b': 0, 'z': 0, 'c': 0}
 
-transport = sys.argv[1]
-print(f"Transport direction: {transport}")
+parser = argparse.ArgumentParser(description="Compute LDOS from TBTrans Calculation")
+parser.add_argument("syslabel", type=str,
+                    help="System Label before file endings (e.g. LABEL for LABEL.fdf, LABEL.TBT.nc)")
+parser.add_argument("transport_direction", type=str, choices=["x", "y", "z", "a", "b", "c"],
+                    help="Transport direction (xyz or abc)")
+parser.add_argument("vacuum_direction", type=str, choices=["x", "y", "z", "a", "b", "c"],
+                    help="Vacuum direction (xyz or abc)")
 
+# Parse arguments
+args = parser.parse_args()
+syslabel = args.syslabel
+transport = dir_map[args.transport_direction]
+vacuum = dir_map[args.vacuum_direction]
+
+if transport == vacuum:
+    print("Transport and Vacuum direction cannot be same")
+
+ave = ({0, 1, 2} - {transport, vacuum}).pop()   # Direction that is not transport or vacuum
 
 # TBTrans output file
-tbt = sisl.io.tbtrans.tbtncSileTBtrans("Device.TBT.nc")
+tbt = sisl.io.tbtrans.tbtncSileTBtrans(syslabel + ".TBT.nc")
 # FDF input file with geometry, basis and lattice information
-fdf = sisl.io.siesta.fdfSileSiesta("Device.fdf")
+fdf = sisl.io.siesta.fdfSileSiesta(syslabel + ".fdf")
 
 
 # Get grid information
@@ -20,20 +36,26 @@ grid_template = fdf.read_grid("ElectrostaticPotential")
 grid_template.fill(0)
 shape = grid_template.shape
 
-
 # Setup geometry information for grid
 geo = fdf.read_geometry()
-geo.set_nsc(geo.find_nsc()) # Set optimal supercells for orbital calculations
+geo.set_nsc(geo.find_nsc())  # Set optimal supercells for orbital calculations
 
-a1 = geo.cell[0]; dx = shape[0] / np.linalg.norm(a1)
-a2 = geo.cell[1]; dy = shape[1] / np.linalg.norm(a2)
-a3 = geo.cell[2]; dz = shape[2] / np.linalg.norm(a3)
+a1 = geo.cell[0]
+a2 = geo.cell[1]
+a3 = geo.cell[2]
+dx = shape[0] / np.linalg.norm(a1)
+dy = shape[1] / np.linalg.norm(a2)
+dz = shape[2] / np.linalg.norm(a3)
 
+d = (dx, dy, dz)
+
+print(geo)
 print(f"Lattice Cell: \n{geo.cell}")
 print(f"Grid Shape: {shape}") 
 
 # Get energy resolved density matrix (DM)
 density_matrices = [tbt.density_matrix(float(E_pt), kavg=True, geometry=geo) for E_pt in tbt.E]
+
 
 # Projects energy resolved DM to real space densities
 def compute_ldos(args):
@@ -47,6 +69,7 @@ def compute_ldos(args):
 
     return temp_grid.grid, E_pt
 
+
 if __name__ == "__main__":
 
     # Limited memory bandwidth, deps on the grid spacings, size
@@ -57,7 +80,7 @@ if __name__ == "__main__":
     arg_list = list(zip(density_matrices, tbt.E))
     print(f"Total Energies: {tbt.nE}", flush=True)
     with mp.Pool(processes=num_cores) as pool:
-        results = pool.map(compute_ldos,arg_list)
+        results = pool.map(compute_ldos, arg_list)
 
     calc, E = zip(*results)
     E = np.array(E)
@@ -65,17 +88,9 @@ if __name__ == "__main__":
     sort_calc = [calc[i] for i in sort_indices]
     sort_E = E[sort_indices]
 
-    if transport == 'y':
-        # x-summed grid, LDOS(y,z,E)
-        ldos_grids = np.zeros((grid_template.shape[1], grid_template.shape[2], tbt.nE), dtype = np.float64)
-        for idx, grid_data in enumerate(sort_calc):
-            ldos_grids[:,:,idx] = np.sum(grid_data, axis = 0) * dx
-    else:
-        # y-summed grid, LDOS(x,z,E)
-        ldos_grids = np.zeros((grid_template.shape[0], grid_template.shape[2], tbt.nE), dtype = np.float64)
-        for idx, grid_data in enumerate(sort_calc):
-            ldos_grids[:,:,idx] = np.sum(grid_data, axis = 1) * dy
-
+    ldos_grids = np.zeros((grid_template.shape[transport], grid_template.shape[vacuum], tbt.nE), dtype=np.float64)
+    for idx, grid_data in enumerate(sort_calc):
+        ldos_grids[:, :, idx] = np.sum(grid_data, axis=ave) * d[ave]
 
     np.save("ldos_arr_par.npy", ldos_grids)
     #p = grid_data.plot.grid(axes = 'xz', show_cell = False)
